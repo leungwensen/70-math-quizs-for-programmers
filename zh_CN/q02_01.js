7de521fab75d5ad8bfc4cf518285c8a3da54b83d
leungwensen <leungwensen@gmail.com>
2016-04-17 19:49:56 +0800

    finishing q02

--- zh_CN/q02_01.js
+++ zh_CN/q02_01.js
@@ -6,7 +6,7 @@ for (i = 1000; i < 10000; i++){
       for (l = 0; l < op.length; l++){
         val = c.charAt(3) + op[j] + c.charAt(2) + op[k] +
               c.charAt(1) + op[l] + c.charAt(0);
-        if (val.length > 4){ /* 必ず1つは演算子を入れる */
+        if (val.length > 4){ /* 一定要插入1个运算符 */
           if (i == eval(val)){
             console.log(val + " = " + i);
           }
--- zh_CN/q02_02.php
+++ zh_CN/q02_02.php
@@ -2,12 +2,12 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>電卓</title>
+<title>计算器</title>
 </head>
 <body>
 <form method="post" action="<?php echo $_SERVER['SCRIPT_NAME'];?>">
 <input type="text" name="exp" size="30">
-<input type="submit" value="計算">
+<input type="submit" value="计算">
 </form>
 <div>
 <?php
