2a5301659a3ae214e64fb2c2f109768b90727f0c
leungwensen <leungwensen@gmail.com>
2016-05-17 23:05:25 +0800

    finishing Q17

--- zh_CN/q16_01.rb
+++ zh_CN/q16_01.rb
@@ -1,13 +1,13 @@
 MAX = 500
 
 answer = []
-(1..MAX/4).each{|c|           # 正方形の一辺
+(1..MAX/4).each{|c|           # 正方形边长
   edge = (1..(c-1)).to_a.map{|tate| tate * (2 * c - tate)}
-  edge.combination(2){|a, b|  # 長方形の面積
+  edge.combination(2){|a, b|  # 长方形面积
     if a + b == c * c then
       answer.push([1, b / a.to_f, c * c / a.to_f])
     end
   }
 }
-answer.uniq!                  # 整数倍を除外
+answer.uniq!                  # 去除整数倍的情况
 puts answer.size
--- zh_CN/q16_02.rb
+++ zh_CN/q16_02.rb
@@ -1,7 +1,7 @@
 MAX = 500
 
 cnt = 0
-(1..MAX/4).each{|c|      # 正方形の一辺
+(1..MAX/4).each{|c|      # 正方形边长
   (1..(c-1)).to_a.combination(2){|a, b|
     if a * a + b * b == c * c then
       cnt += 1 if a.gcd(b) == 1
