ebc97a968c237447e410f32a32352fec8bc33e20
leungwensen <leungwensen@gmail.com>
2016-05-23 09:52:37 +0800

    finishing q19

--- zh_CN/q19_01.rb
+++ zh_CN/q19_01.rb
@@ -1,15 +1,15 @@
 require 'prime'
 
-primes = Prime.take(6)        # a〜fに当てはまる素数6個
-min = primes[-1] * primes[-1] # 最大でも一番大きいものの平方
+primes = Prime.take(6)        # 用6个质数充当a~f
+min = primes[-1] * primes[-1] # 把最小数字设置成最大质数的平方
 min_friend = []
 
-primes.permutation{|prime|    # 6個の素数の順列
-  # 2つずつ選んで掛け算する
+primes.permutation{|prime|    # 按顺序排列的6个质数
+  # 按顺序选取2个数字做乘法
   friends = prime.each_cons(2).map{|x, y| x * y}
-  # 先頭と末尾は同じ数の平方
+  # 开头和结尾的数字乘方
   friends += [prime[0], prime[-1]].map{|x| x * x}
-  if min > friends.max then   # 最小を更新した場合
+  if min > friends.max then   # 更新最小数字的情况
     min = friends.max
     min_friend = friends
   end
