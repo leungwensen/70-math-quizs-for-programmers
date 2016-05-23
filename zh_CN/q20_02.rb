7607c16f3f5b43ebacd8d428331bd106d9420ef9
leungwensen <leungwensen@gmail.com>
2016-05-24 00:26:50 +0800

    finishing q20

--- zh_CN/q20_02.rb
+++ zh_CN/q20_02.rb
@@ -1,18 +1,18 @@
-# 魔方陣を配列にセット
+# 把魔方阵保存到数组
 magic_square = [1, 14, 14, 4, 11, 7, 6, 9,
                 8, 10, 10, 5, 13, 2, 3, 15]
 sum_all = magic_square.inject(:+)
 
-# 集計用のハッシュ
+# 统计用哈希表
 sum = Array.new(sum_all + 1){0}
-# 初期値（何も足さないときが1個）
+# 初始值（没有加任何值时）
 sum[0] = 1
 magic_square.each{|n|
-  # 大きい方から順に加算
+  # 从大数开始按顺序做加法
   (sum_all - n).downto(0).each{|i|
     sum[i + n] += sum[i]
   }
 }
 
-# 合計が最大のものを出力
+# 输出出现次数最多的和值
 puts sum.find_index(sum.max)
