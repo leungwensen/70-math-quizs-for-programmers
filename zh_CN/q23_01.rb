8755c2f8d0dc66a7b651a4026f67154bba08e9ad
leungwensen <leungwensen@gmail.com>
2016-05-26 23:03:48 +0800

    finishing q23

--- zh_CN/q23_01.rb
+++ zh_CN/q23_01.rb
@@ -4,8 +4,8 @@ def game(coin, depth)
   return @memo[[coin, depth]] if @memo.has_key?([coin, depth])
   return 0 if coin == 0
   return 1 if depth == 0
-  win = game(coin + 1, depth - 1)  # 勝ったとき
-  lose = game(coin - 1, depth - 1) # 負けたとき
+  win = game(coin + 1, depth - 1)  # 获胜时
+  lose = game(coin - 1, depth - 1) # 落败时
   @memo[[coin, depth]] = win + lose
 end
 
