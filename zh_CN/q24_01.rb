544fce67d91d0877c4a75fb27d549c8f31f05969
leungwensen <leungwensen@gmail.com>
2016-05-28 00:26:22 +0800

    finishing q24

--- zh_CN/q24_01.rb
+++ zh_CN/q24_01.rb
@@ -1,18 +1,18 @@
-# 2枚抜きの可能性がある抜き方をセット
+# 列举能一次击落的2个靶子的组合
 board = [[1, 2], [2, 3], [7, 8], [8, 9],
          [1, 4], [3, 6], [4, 7], [6, 9]]
-# 1枚ずつの抜き方を追加
+# 增加逐个击落的方法
 1.upto(9){|i|
   board.push([i])
 }
 
 @memo = {[] => 1}
 def strike(board)
-  # すでに探索済みの場合はその値を使用
+  # 如果已经全部遍历完，则输出这个值
   return @memo[board] if @memo.has_key?(board)
   cnt = 0
   board.each{|b|
-    # 抜いた的に含まれる数字がある抜き方は除外
+    # 排除含有已经击落数字的组合
     next_board = board.select{|i| (b & i).size == 0}
     cnt += strike(next_board)
   }
