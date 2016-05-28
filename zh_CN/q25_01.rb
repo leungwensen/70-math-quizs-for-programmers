ad8e19e29ceb18fcd95f8b5d51e9e879ff889962
leungwensen <leungwensen@gmail.com>
2016-05-28 18:23:49 +0800

    finishing q25

--- zh_CN/q25_01.rb
+++ zh_CN/q25_01.rb
@@ -1,9 +1,9 @@
 N = 6
 
 max_cnt = 0
-(1..N - 1).to_a.permutation(N - 1){|l|   # 左側の順番
-  (1..N - 1).to_a.permutation(N - 1){|r| # 右側の順番
-    # 経路を設定
+(1..N - 1).to_a.permutation(N - 1){|l|   # 左侧的顺序
+  (1..N - 1).to_a.permutation(N - 1){|r| # 右侧的顺序
+    # 设置连线
     path = []
     left = 0
     right = r[0]
@@ -15,7 +15,7 @@ max_cnt = 0
     }
     path.push([left, 0])
 
-    # 経路が交差しているかを判定
+    # 判断连线是否相交
     cnt = 0
     (N * 2 - 1).times{|i|
       (i + 1).upto(N * 2 - 2){|j|
