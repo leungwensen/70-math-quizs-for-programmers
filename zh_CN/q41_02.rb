adfdc5609c3e98af6e39cd46bdbb3c8f3e233116
leungwensen <leungwensen@gmail.com>
2016-06-11 18:08:27 +0800

    finishing q41

--- zh_CN/q41_02.rb
+++ zh_CN/q41_02.rb
@@ -1,19 +1,19 @@
 val = []
 256.times{|i|
-  # 0〜255で反転
+  # 反转0~255
   rev = ('%08b'%i).reverse.to_i(2)
 
   if i < rev then
     s = i.to_s + rev.to_s
-    # 0〜9が重複しない数字であれば対象とする
+    # 如果使用了0~9这10个数字个一次，就符合条件
     val.push([i, rev]) if s.split('').uniq.size == s.length
   end
 }
 
 ip = []
 val.combination(2){|a, b|
-  # 0〜9を一度ずつ使っていればペアとする
+  # 使用了0~9这10个数字个一次，就形成分组
   ip.push([a, b]) if (a + b).join.split('').uniq.size == 10
 }
-# ペアの組み合わせ数を出力
+# 组合各分组输出结果
 puts ip.size * 8
