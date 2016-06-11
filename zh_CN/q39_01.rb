41e3c050aebed4401abaae5af025146c9368a92c
leungwensen <leungwensen@gmail.com>
2016-06-11 14:53:37 +0800

    finishing q39

--- zh_CN/q39_01.rb
+++ zh_CN/q39_01.rb
@@ -1,4 +1,4 @@
-# 反転するマスクを設定
+# 设置反转用的掩码
 mask = Array.new(16)
 4.times{|row|
   4.times{|col|
@@ -8,18 +8,18 @@ mask = Array.new(16)
 }
 
 max = 0
-# ステップ数を保存する配列
+# 保存步骤数目的数组
 steps = Array.new(1 << 16, -1)
-# すべて白からスタート
+# 从所有方格都为白色开始
 steps[0] = 0
-# 調査対象の配列
+# 检查对象的数组
 scanner = [0]
 while scanner.size > 0 do
   check = scanner.shift
   next_steps = steps[check] + 1
   16.times{|i|
     n = check ^ mask[i]
-    # 未チェックの場合、さらに調査する
+    # 如果未检查过，则进一步检索
     if steps[n] == -1 then
       steps[n] = next_steps
       scanner.push(n)
@@ -28,6 +28,6 @@ while scanner.size > 0 do
   }
 end
 
-puts max # 最大ステップ数
-puts steps.index(max).to_s(2) # 初期状態のマス目：すべて黒
-p steps.select{|i| i == -1} # 白にならない初期状態は無い
+puts max # 最大步骤数
+puts steps.index(max).to_s(2) # 初始状态的方格：全部黑色
+p steps.select{|i| i == -1} # 不存在不能全部变为白色的初始状态
