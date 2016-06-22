15b9c39798b8ed375b95b3b2d0dc6e3f66d8fc24
leungwensen <leungwensen@gmail.com>
2016-06-23 01:21:47 +0800

    finishing q44

--- zh_CN/q44_01.rb
+++ zh_CN/q44_01.rb
@@ -1,9 +1,9 @@
 def search(abc, depth, max_abc, log)
-  return false if log.has_key?(abc)        # 探索済み
-  return true if abc[0] == max_abc[0] / 2  # 終了条件
+  return false if log.has_key?(abc)        # 搜索完成
+  return true if abc[0] == max_abc[0] / 2  # 终止条件
   log[abc] = depth
   [0, 1, 2].permutation(2).each{|i, j|
-    # A, B, Cのうち移動する2つを選択
+    # 从A,B,C中选择2个开始倒水
     if (abc[i] > 0) || (abc[j] < max_abc[j])
       next_abc = abc.clone
       move = [abc[i], max_abc[j] - abc[j]].min
@@ -19,7 +19,7 @@ cnt = 0
 10.step(100, 2){|a|
   (1..(a/2 - 1)).each{|c|
     b = a - c
-    if b.gcd(c) == 1 then # 互いに素の場合は最大公約数＝1
+    if b.gcd(c) == 1 then # 互质，也就是最大公约数为1
       cnt += 1 if search([a, 0, 0], 0, [a, b, c], {})
     end
   }
