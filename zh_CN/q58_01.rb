d1b43b801e95eea058c7a3cc729415ede5d0771c
leungwensen <leungwensen@gmail.com>
2016-07-18 22:41:57 +0800

    q58

--- zh_CN/q58_01.rb
+++ zh_CN/q58_01.rb
@@ -1,26 +1,26 @@
 n = 14
-# 初期状態の人数をセット(a, b, cの人数 ＋ 先生が電話した回数)
+# 设置初始状态人数(a, b, c的人数+教师通话次数)
 status = [[n, 0, 0, 0]]
-step = 0 # 経過時間
+step = 0 # 经过时间
 while status.select{|s| s[1] == n}.size == 0 do
-  # 連絡が不要な生徒(b)が全員になるまで以下を繰り返す
+  # 循环处理，直到不必通话的学生(b)人数变为总人数
   next_status = []
   status.each{|s|
     (s[1] + 1).times{|b|
-      # 連絡が不要な生徒が他の生徒に連絡する人数
+      # 不必通话的学生联系其他学生的人数
       (s[2] + 1).times{|c|
-        # 連絡が必要な生徒が連絡する人数
-        if s[2] > 0 then # 発信できる生徒がいるとき
-          # 生徒から先生あり
+        # 需要通话的学生联系的人数
+        if s[2] > 0 then # 有可通话学生的时候
+          # 有学生联系教师
           if s[0]-b-c+1 >= 0 then
             next_status << [s[0]-b-c+1, s[1]+c, s[2]+b-1, s[3]+1]
           end
         end
-        #先生なし
+        # 没有学生联系教师
         if s[0]-b-c >= 0 then
           next_status << [s[0]-b-c, s[1]+c, s[2]+b, s[3]]
         end
-        # 先生から生徒あり
+        # 教师联系了学生
         if s[0]-b-c-1 >= 0 then
           next_status << [s[0]-b-c-1, s[1]+c+1, s[2]+b, s[3]+1]
         end
@@ -30,7 +30,7 @@ while status.select{|s| s[1] == n}.size == 0 do
   status = (next_status - status).uniq
   step += 1
 end
-# 経過時間を表示
+# 打印经过时间
 puts step
-# 最短の中で、先生が電話する回数が最少のものを表示
+# 打印在最短时间的情况中，教师通话次数最少的情况
 p status.select{|s| s[1] == n}.min{|a, b| a[3] <=> b[3]}
