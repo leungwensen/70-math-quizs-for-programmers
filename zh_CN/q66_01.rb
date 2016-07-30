32317d4a0817d8ae96edec64975bd67d6536ec29
leungwensen <leungwensen@gmail.com>
2016-07-30 17:07:47 +0800

    q66

--- zh_CN/q66_01.rb
+++ zh_CN/q66_01.rb
@@ -1,26 +1,26 @@
-# パネルの数を設定
+# 设置图块数目
 W, H = 4, 3
-# ビット反転用の値
+# 按位反转用的值
 XOR_ROW = (1 << (W + 1)) - 1
 
-# 行単位に探索
+# 按行搜索
 def search(up, y, odds)
-  # 上の行までに奇数の数が2つより多い場合は対象外
+  # 截至上一行，如果奇数顶点的数目大于2，则排除这种情况
   return 0 if 2 < odds
 
-  row = 1 << W | 1     # 初期値をセット
+  row = 1 << W | 1     # 设置初始值
 
-  # 最初と最後の行は反転
+  # 翻转最初和最后的行
   row = XOR_ROW ^ row if (y == 0) || (y == H)
 
-  if y == H then       # 最終行の場合はチェックして終了
-    odds += (row ^ up).to_s(2).count("1")   # 奇数の数をカウント
-    return 1 if (odds == 0) || (odds == 2)  # 0か2個なら対象
+  if y == H then       # 如果是最后一行，则检查后结束
+    odds += (row ^ up).to_s(2).count("1")   # 计算奇数个数
+    return 1 if (odds == 0) || (odds == 2)  # 如果为0或者2，则计入结果
     return 0
   end
   cnt = 0
-  (1 << W).times{|a|   # パネルの内容（左上から右下の直線有無）
-    (1 << W).times{|b| # パネルの内容（左下から右上の直線有無）
+  (1 << W).times{|a|   # 图块内容（有无左上至右下的线条）
+    (1 << W).times{|b| # 图块内容（有无左下至右上的线条）
       cnt += search(a ^ b << 1, y + 1,
                     odds + (row ^ up ^ a << 1 ^ b).to_s(2).count("1"))
     }
--- zh_CN/q66_02.rb
+++ zh_CN/q66_02.rb
@@ -1,38 +1,39 @@
-# パネルの数を設定
+# 设置图块数目
 W, H = 4, 3
 row = [0] + [1] * (W - 1) + [0]
 @edge = row + row.map{|r| 1 - r} * (H - 1) + row
 
 def search(panel, odds)
-  # 最後のパネルをセットしたときに奇数点が2個を超えるか
+  # 截至最后一个图块，奇数顶点是否超过2个
   return (@edge.inject(:+) > 2)?0:1 if panel >= (W + 1) * H
   # 途中で奇数点が2個を超えると一筆書き不可
+  # 如果中途奇数顶点超过2个，则不可能完成一笔画
   return 0 if odds > 2
 
   cnt = 0
-  if panel % (W + 1) < W then   # 行の右端以外
-    # パネル内に斜線がない場合を探索
+  if panel % (W + 1) < W then   # 到达行的最右侧
+    # 遍历图块内没有斜线的情况
     cnt += search(panel + 1, odds + @edge[panel])
 
-    # パネルの左上から右下への線
+    # 图块有从左上到右下的线
     @edge[panel] = 1 - @edge[panel]
     @edge[panel + W + 2] = 1 - @edge[panel + W + 2]
     cnt += search(panel + 1, odds + @edge[panel])
 
-    # パネルをクロスする線
+    # 图块有交叉线
     @edge[panel + 1] = 1 - @edge[panel + 1]
     @edge[panel + W + 1] = 1 - @edge[panel + W + 1]
     cnt += search(panel + 1, odds + @edge[panel])
 
-    # パネルの右上から左下への線
+    # 图块有从右上到左下的线
     @edge[panel] = 1 - @edge[panel]
     @edge[panel + W + 2] = 1 - @edge[panel + W + 2]
     cnt += search(panel + 1, odds + @edge[panel])
 
-    # 斜線を元に戻す
+    # 斜线回到原点
     @edge[panel + 1] = 1 - @edge[panel + 1]
     @edge[panel + W + 1] = 1 - @edge[panel + W + 1]
-  else               # 行の右端の場合、次の行へ
+  else               # 到达行右端时，进入下一行
     cnt += search(panel + 1, odds + @edge[panel])
   end
   cnt
