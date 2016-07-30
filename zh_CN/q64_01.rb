c819909cf942a448d16e33b368c625260d36b80b
leungwensen <leungwensen@gmail.com>
2016-07-30 14:52:50 +0800

    q64

--- zh_CN/q64_01.rb
+++ zh_CN/q64_01.rb
@@ -1,43 +1,42 @@
 N = 5
-# 右手法の移動方向（右、上、左、下の順）
+# 右手法则的移动方向（按右、上、左、下的顺序）
 @dx = [[1, 0], [0, -1], [-1, 0], [0, 1]]
 
-# maze:壁の配置
-# p1, d1: 1人目の経路、移動方向
-# p2, d2: 2人目の経路、移動方向
+# maze: 墙壁设置
+# p1, d1: 第1个人走过的路径和移动方向
+# p2, d2: 第2个人走过的路径和移动方向
 def search(maze, p1, d1, p2, d2)
-  if p1.size == p2.size then # 2人がともに移動した場合
-    # 2人が出会った場合は成功
-    return true if p1[-1][0..1] == p2[-1][0..1]
-    # 1人目が右下に着いた場合は失敗
-    return false if p1[-1][0..1] == [N - 1, N - 1]
-    # 2人目が左上に着いた場合も失敗
-    return false if p2[-1][0..1] == [0, 0]
-  end
-  # 同じ方向から移動してきた場合はループなので失敗
-  return false if p1.count(p1[-1]) > 1
-
-  pre = p1[-1]
-  @dx.size.times{|i| # 右手法で動ける方向を探索
-    d = (d1 - 1 + i) % @dx.size
-    px = pre[0] + @dx[d][0]
-    py = pre[1] + @dx[d][1]
-    # 移動先が壁になっていないかチェック
-    if (px >= 0) && (px < N) && (py >= 0) && (py < N) &&
-       (maze[px + N * py] == 0) then
-      return search(maze, p2, d2, p1 + [[px, py, d]], d)
-      break
+    if p1.size == p2.size then # 两人同时移动的情况
+        # 两人相遇则成功
+        return true if p1[-1][0..1] == p2[01][0..1]
+        # 第1个人到达右下方则失败
+        return false if p1[-1][0..1] == [N - 1, N - 1]
+        # 第2个人到达左上方则失败
+        return false if p2[-1][0..1] == [0, 0]
     end
-  }
-  false
+    # 两人往同一个方向移动则移动方向形成环，失败
+    return false if p1.count(p1[-1]) > 1
+
+    pre = p1[-1]
+    @dx.size.times{|i| # 搜索右手法则指定的方向
+        d = (d1 - 1 + i) % @dx.size
+        px = pre[0] + @dx[d][0]
+        py = pre[1] + @dx[d][1]
+        # 判断移动前方是否是墙壁
+        if (px >= 0) && (px < N) && (py >= 0) && (py < N) && (maze[px + N * py] == 0) then
+            return search(maze, p2, d2, p1 + [[px, py, d]], d)
+            break
+        end
+    }
+    false
 end
 
-a = [[0, 0, -1]]         # A:左上(X座標、Y座標、前の移動方向)
-b = [[N - 1, N - 1, -1]] # B:右下(X座標、Y座標、前の移動方向)
+a = [[0, 0, -1]]            # A: 左上角（X坐标，Y坐标、向前的移动方向）
+b = [[N - 1, N - 1, -1]]    # B: 右下角（X坐标，Y坐标、向前的移动方向）
 cnt = 0
 [0, 1].repeated_permutation(N * N - 2){|maze|
-  # 2人の開始位置は必ず通路として探索
-  # Aは下に移動(@dx[3])、Bは上に移動(@dx[1])
-  cnt += 1 if search([0] + maze + [0], a, 3, b, 1)
+    # 两人的起始位置一定作为路径的一部分检索
+    # A向下移动（@dx[3]）、B向上移动（@dx[1]）
+    cnt += 1 if search([0] + maze + [0], a, 3, b, 1)
 }
 puts cnt
--- zh_CN/q64_02.rb
+++ zh_CN/q64_02.rb
@@ -1,48 +1,48 @@
 N = 5
 MASK = (1 << (N * N)) - 1
-# 移動した位置をビット演算で算出
+# 利用位运算计算已经移动的位置
 @move = [lambda{|m| (m >> 1) & 0b0111101111011110111101111},
          lambda{|m| (m << N) & MASK},
          lambda{|m| (m << 1) & 0b1111011110111101111011110},
          lambda{|m| m >> N}]
 
-# 有効な迷路かを判定
+# 判断有效路径
 def enable(maze)
-  man = (1 << (N * N - 1)) & (MASK - maze) # 左上からスタート
-  while true do
-    next_man = man
-    @move.each{|m| next_man |= m.call(man)} # 上下左右に移動
-    next_man &= (MASK - maze)        # 壁以外の部分が移動可能
-    return true if next_man & 1 == 1 # 右下にたどり着けば有効
-    break if man == next_man
-    man = next_man
-  end
-  false
+    man = (1 << (N * N - 1)) & (MASK - maze)    # 从左上角出发
+    while true do
+        next_man = man
+        @move.each{|m| next_man |= m.call(man)} # 上下左右移动
+        next_man &= (MASK - maze)               # 可以移动到墙壁以外的方格
+        return true if next_man & 1 == 1        # 到达右下角有效
+        break if man == next_man
+        man = next_man
+    end
+    false
 end
 
-# map:壁の配置
-# p1, d1: 1人目の位置、移動方向
-# p2, d2: 2人目の位置、移動方向
+# maze: 墙壁设置
+# p1, d1: 第1个人走过的路径和移动方向
+# p2, d2: 第2个人走过的路径和移动方向
 def search(maze, p1, d1, p2, d2, turn)
-  if turn then
-    return true if p1 == p2 # 2人が出会った
-    # どちらかのゴールに着いた
-    return false if (p1 == 1) || (p2 == 1 << (N * N - 1))
-  end
-  @move.size.times{|i| # 右手法で動ける方向を探索
-    d = (d1 - 1 + i) % @move.size
-    if @move[d].call(p1) & (MASK - maze) > 0 then
-      return search(maze, p2, d2, @move[d].call(p1), d, !turn)
+    if true then
+        return true if p1 == p2 # 两人相遇
+        # 其中一人到达目标
+        return false if (p1 == 1) || (p2 == 1 << (N * N - 1))
     end
-  }
-  false
+    @move.size.times{|i| # 搜索右手法则指定的方向
+        d = (d1 - 1 + i) % @move.size
+        if @move[d].call(p1) & (MASK - maze) > 0 then
+            return search(maze, p2, d2, @move[d].call(p1), d, !turn)
+        end
+    }
+    false
 end
 
 cnt = 0
 (1 << N * N).times{|maze|
-  if enable(maze) then
-    man_a, man_b = 1 << (N * N - 1), 1
-    cnt += 1 if search(maze, man_a, 3, man_b, 1, true)
-  end
+    if enable(maze) then
+        man_a, man_b = 1 << (N * N - 1), 1
+        cnt += 1 if search(maze, man_a, 3, man_b, 1, true)
+    end
 }
 puts cnt
