34cd236a75dfef641a7bdec36f52fc7fd34e84a6
leungwensen <leungwensen@gmail.com>
2016-07-30 16:12:34 +0800

    q65

--- zh_CN/q65_01.rb
+++ zh_CN/q65_01.rb
@@ -1,20 +1,20 @@
-# ペアは6組
+# 6组对手
 PAIR = 6
 
-# 開始と終了を設定
+# 设置起始和终止状态
 start = (1..PAIR * 2 - 1).to_a + [0]
 goal = [0] + (2..PAIR * 2 - 1).to_a + [1]
 
-# 投げられる一覧を取得
+# 获取投接球状态列表
 def throwable(balls)
   result = []
   balls.each{|ball|
-    c = ball.index(0)                     # 受け手の位置を取得
-    p = (c + PAIR) % (PAIR * 2)           # 受け手の正面を計算
-    [-1, 0, 1].each{|d|                   # 正面と左右
+    c = ball.index(0)                     # 获取接球手位置
+    p = (c + PAIR) % (PAIR * 2)           # 计算接球手对面位置
+    [-1, 0, 1].each{|d|                   # 正对面及其左右
       if (p + d) / PAIR == p / PAIR then
         ball[c], ball[p + d] = ball[p + d], ball[c]
-        result.push(ball.clone)           # 投げた結果をセット
+        result.push(ball.clone)           # 设置投球结果
         ball[c], ball[p + d] = ball[p + d], ball[c]
       end
     }
@@ -22,15 +22,15 @@ def throwable(balls)
   result
 end
 
-# 初期状態を設定
+# 设定初始状态
 balls = [start]
 log = [start]
 cnt = 0
-# 幅優先探索を実行
+# 广度优先搜索
 while !balls.include?(goal) do
-  next_balls = throwable(balls)  # 次のステップを取得
-  balls = next_balls - log       # 過去に現れていないものを選択
-  log |= next_balls              # 投げた結果を追加
+  next_balls = throwable(balls)  # 获取下一步
+  balls = next_balls - log       # 选择之前没有出现过的投球方案
+  log |= next_balls              # 添加投球结果
   cnt += 1
 end
 puts cnt
--- zh_CN/q65_02.rb
+++ zh_CN/q65_02.rb
@@ -1,20 +1,20 @@
-# ペアは6組
+# 6组对手
 PAIR = 6
 
-# 開始と終了を設定
+# 设置起始和终止状态
 start = (1..PAIR * 2 - 1).to_a + [0]
 goal = [0] + (2..PAIR * 2 - 1).to_a + [1]
 
-# 投げられる一覧を取得
+# 获取投接球状态列表
 def throwable(balls)
   result = []
   balls.each{|ball|
-    c = ball.index(0)                     # 受け手の位置を取得
-    p = (c + PAIR) % (PAIR * 2)           # 受け手の正面を計算
-    [-1, 0, 1].each{|d|                   # 正面と左右
+    c = ball.index(0)                     # 获取接球手位置
+    p = (c + PAIR) % (PAIR * 2)           # 计算接球手对面位置
+    [-1, 0, 1].each{|d|                   # 正对面及其左右
       if (p + d) / PAIR == p / PAIR then
         ball[c], ball[p + d] = ball[p + d], ball[c]
-        result.push(ball.clone)           # 投げた結果をセット
+        result.push(ball.clone)           # 设置投球结果
         ball[c], ball[p + d] = ball[p + d], ball[c]
       end
     }
@@ -22,25 +22,25 @@ def throwable(balls)
   result
 end
 
-# 初期状態を設定
+# 设定初始状态
 fw = [start]
 fw_log = [start]
 bw = [goal]
 bw_log = [goal]
 cnt = 0
 
-# 双方向からの幅優先探索を実行
+# 双向的广度优先搜索
 while true do
-  next_fw = throwable(fw)      # 順方向に次のステップを取得
-  fw = next_fw - fw_log        # 過去に現れていないものを選択
-  fw_log |= next_fw            # 投げた結果を追加
+  next_fw = throwable(fw)      # 正向的下一步
+  fw = next_fw - fw_log        # 选择之前没有出现过的投球方案
+  fw_log |= next_fw            # 添加投球结果
   cnt += 1
-  break if (fw & bw).size > 0  # 重なったら終了
+  break if (fw & bw).size > 0  # 如果状态相同，则终止处理
 
-  next_bw = throwable(bw)      # 逆方向に次のステップを取得
-  bw = next_bw - bw_log        # 過去に現れていないものを選択
-  bw_log |= next_bw            # 投げた結果を追加
+  next_bw = throwable(bw)      # 反向的下一步
+  bw = next_bw - bw_log        # 选择之前没有出现过的投球方案
+  bw_log |= next_bw            # 添加投球结果
   cnt += 1
-  break if (fw & bw).size > 0  # 重なったら終了
+  break if (fw & bw).size > 0  # 如果状态相同，则终止处理
 end
 puts cnt
